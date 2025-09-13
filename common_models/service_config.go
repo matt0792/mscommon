2c1dd362fe331edd3c3b9d7285b7b810048e2ca3
Matt Thompson <matthew.thompson6@icloud.com>
2025-09-14 00:38:48 +0200

    misc'

--- common_models/service_config.go
+++ common_models/service_config.go
@@ -1,7 +1,14 @@
 package commonmodels
 
 type MicroserviceConfig struct {
-	ServiceLocation string
-	ServiceID       string
-	ServiceName     string
+	ID              string `bson:"_id,omitempty" json:"id"`
+	ServiceLocation string `bson:"service_location" json:"serviceLocation"`
+	ServiceID       string `bson:"service_id" json:"serviceId"`
+	ServiceName     string `bson:"service_name" json:"name"`
+}
+
+type MicroserviceConfigInput struct {
+	ServiceLocation string `bson:"service_location" json:"serviceLocation"`
+	ServiceID       string `bson:"service_id" json:"serviceId"`
+	ServiceName     string `bson:"service_name" json:"name"`
 }
